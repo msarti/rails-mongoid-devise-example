d58105c79827236d61bfe9f7df054254ae4b9e1a
Marco <marco@sartimarco.it>
2012-04-25 01:03:47 +0200

    MongoID and devise integration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMongoidDeviseExample::Application.config.secret_token = 'eac57315f96f46bba867e8b00d199fd735f1bf7d0e421e7581f6204357d78995401d5928b857497ef93dd23f11d363ea65544c3908a30387e013244f73fd9d5c'
